f840fa0cfb0552dbfdcf87087fac4ace5d0766cd
Pavan2806 <102680825+Pavan2806@users.noreply.github.com>
2024-08-22 21:59:24 +0530

    Add files via upload

---
+++ Week1day3/Student.java
@@ -0,0 +1,23 @@
+package javaLearning;
+
+public class Student {
+	public  void learnJava() {
+		System.out.println("Student is learning java");
+	}
+	public void playingCricket() {
+		System.out.println("student is playing cricket");
+	}
+	public void  writingExam() {
+		System.out.println("student is writing the exam");
+	}
+	public static void main (String []args) {
+		Student obj=new Student();
+			obj.learnJava();
+			obj.playingCricket();
+			obj.writingExam();
+		
+	}
+	
+
+}
+                         
