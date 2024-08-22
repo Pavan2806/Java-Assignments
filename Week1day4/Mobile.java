feb3c4cf4efc948bf40ba504e9682cc6283dd108
Pavan2806 <102680825+Pavan2806@users.noreply.github.com>
2024-08-22 22:01:58 +0530

    Add files via upload

---
+++ Week1day4/Mobile.java
@@ -0,0 +1,39 @@
+package javaLearning;
+
+/*Day 4
+  datatypes*/
+
+public class Mobile {
+	public static void main (String []args) {
+		
+		String mobile="Nothing";
+		String model="phone2A";
+		int price=22000;
+		char logo='N';
+		float discount=49.99f;
+		double SARvalue=4.443544534d;
+		long IMEInumber=43754534454764l;
+		boolean expensive=true;
+		System.out.println("Mobile:"+mobile);
+		System.out.println("Model:"+model);
+		System.out.println("Price:"+price);
+		System.out.println("Logo:"+logo);
+		System.out.println("Discount"+discount);
+		System.out.println("SARValue:"+SARvalue);
+		System.out.println("IMEInumber:"+IMEInumber);
+		System.out.println("Expensive:"+expensive);
+		System.out.println("Mobile:"+mobile+" "+"Model:"+model+" "+"Price:"+price+" "+"Logo:"+logo+" "+"Discount"+discount+" " +"SARValue:"+SARvalue+" " +"IMEInumber:"+IMEInumber+" "+"Expensive:"+expensive);
+		
+	}
+
+}
+	
+
+		
+	           
+	        		   
+	        		   
+	           
+
+
+
